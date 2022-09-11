f5fa6cd830427f3c45755e9cee9a46848f997dbe
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-09-11 15:36:30 -0600

    add navegation & images

---
+++ app/src/main/java/com/example/lab8/CharactersFragment.kt
@@ -0,0 +1,15 @@
+package com.example.lab8
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CharactersFragment : Fragment(R.layout.fragment_characters) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/lab8/DetailsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.lab8
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
